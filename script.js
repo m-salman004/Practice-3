9783a7ebe8569442a53c015a489bb3e9f4982ea0
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2021-12-29 10:19:45 -0800

    Toastify Error

--- script.js
+++ script.js
@@ -14,6 +14,23 @@ function showOutput(outputResult){
 function inputValue(){
     return document.getElementById("inputValue").value
 }
+// Toastify Error
+function error(innerText){
+    Toastify({
+        text : innerText,
+        duration : 3000,
+        newWindow : true,
+        close : true,
+        gravity : "top",
+        position : "center",
+        stopOnFocus : true,
+        style : {
+            background : "#ad5389",
+        },
+        onclick : function() {} 
+    })
+    .showToast();
+}
 
 
 
